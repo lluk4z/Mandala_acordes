53c165595f307578e9e99b7764f835f4039c0aca
Lucas <lemmg@cin.ufpe.br>
2022-07-31 11:17:20 -0300

    mandala dos acordes

---
+++ main.js
@@ -0,0 +1,30 @@
+let container = document.querySelector(".sub-container");
+let container_g = document.querySelector(".container");
+let btn = document.getElementById("spin");
+let btn_prev = document.getElementById("spin-prev");
+
+let btn_g = document.getElementById("spin-g");
+let btn_prev_g = document.getElementById("spin-prev-g");
+let number = 0;
+
+btn.onclick = function () {
+	container.style.transform = "rotate(" + number + "deg)";
+	number += Math.ceil(10);
+}
+
+
+btn_prev.onclick = function () {
+	container.style.transform = "rotate(" + number + "deg)";
+	number -= Math.ceil(10);
+}
+
+
+btn_g.onclick = function () {
+	container_g.style.transform = "rotate(" + number + "deg)";
+	number += Math.ceil(1);
+}
+
+btn_prev_g.onclick = function () {
+	container_g.style.transform = "rotate(" + number + "deg)";
+	number -= Math.ceil(1);
+}
