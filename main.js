d1f432fb3e5c02079f6b31b99d99a6a93da10615
Lucas <lemmg@cin.ufpe.br>
2022-07-31 14:47:05 -0300

    Ajuste no contador

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ let btn_prev = document.getElementById("spin-prev");
 let btn_g = document.getElementById("spin-g");
 let btn_prev_g = document.getElementById("spin-prev-g");
 let number = 0;
+let number_g = 0;
 
 btn.onclick = function () {
 	container.style.transform = "rotate(" + number + "deg)";
@@ -20,11 +21,11 @@ btn_prev.onclick = function () {
 
 
 btn_g.onclick = function () {
-	container_g.style.transform = "rotate(" + number + "deg)";
-	number += Math.ceil(1);
+	container_g.style.transform = "rotate(" + number_g + "deg)";
+	number_g += Math.ceil(1);
 }
 
 btn_prev_g.onclick = function () {
-	container_g.style.transform = "rotate(" + number + "deg)";
-	number -= Math.ceil(1);
+	container_g.style.transform = "rotate(" + number_g + "deg)";
+	number_g -= Math.ceil(1);
 }
